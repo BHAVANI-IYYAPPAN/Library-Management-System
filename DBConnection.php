ca6017d1dad7a848244690733f0ed7f61ada1f48
BHAVANI-IYYAPPAN <111783987+BHAVANI-IYYAPPAN@users.noreply.github.com>
2023-08-23 23:27:21 +0530

    Create DBConnection.php

---
+++ DBConnection.php
@@ -0,0 +1,8 @@
+<?php 
+$conn = new mysqli("localhost","root","","book"); 
+// Check connection 
+if ($conn -> connect_errno) { 
+echo "Failed to connect to MySQL: " . $mysqli -> connect_error; 
+exit(); 
+} 
+?>
